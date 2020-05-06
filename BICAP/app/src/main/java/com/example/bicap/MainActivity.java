ea3250784fa05186a25b423464b2894701c4310b
MicheleRago <michele@rago.ovh>
2020-05-06 14:37:34 +0200

    Home con RecyclerView

--- BICAP/app/src/main/java/com/example/bicap/MainActivity.java
+++ BICAP/app/src/main/java/com/example/bicap/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.bicap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.google.gson.Gson;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //https://code.tutsplus.com/it/tutorials/getting-started-with-recyclerview-and-cardview-on-android--cms-23465
+        RecyclerView rv = (RecyclerView)findViewById(R.id.rv);
+        rv.setHasFixedSize(true); // Se si è certi che le dimensioni del RecyclerView non cambieranno, è possibile aggiungere la seguente stringa per migliorare le prestazioni:
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+
+        LinearLayout mainLinearLayout = (LinearLayout)findViewById(R.id.mainLinearLayout);
+
+
+
+        IndaginiHeadList indaginiHeadList = getIndaginiHeadList();
+
+        RVAdapter adapter = new RVAdapter(indaginiHeadList.getHeads());
+        rv.setAdapter(adapter);
+    }
+
+
+    public IndaginiHeadList getIndaginiHeadList(){
+        IndaginiHeadList indaginiHeadList = new IndaginiHeadList();
+        List<IndagineHead> lista = new ArrayList<IndagineHead>();
+
+        lista.add(new IndagineHead("Colori","Michele Rago", "https://mangadex.org/images/misc/navbar.svg?3", 830616));
+        lista.add(new IndagineHead("Colori","Gianluca Quaglia", "https://mangadex.org/images/misc/navbar.svg?3", 829533));
+        lista.add(new IndagineHead("Droghe","Alessio Villani", "https://mangadex.org/images/misc/navbar.svg?3", 830075));
+
+        indaginiHeadList.setHeads(lista);
+
+        return indaginiHeadList;
     }
 }
