cfe53cc5c0c9f28bfecc5f5fc0dc03218808ec1b
GianQuazzo <q.gian99@quarzo.stream>
2020-05-08 17:23:49 +0200

    cancer

--- BICAP/app/src/main/java/com/example/bicap/IndagineActivity.java
+++ BICAP/app/src/main/java/com/example/bicap/IndagineActivity.java
@@ -1,18 +1,129 @@
 package com.example.bicap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class IndagineActivity extends AppCompatActivity {
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IndagineActivity extends AppCompatActivity implements InformazioneAdapter.OnInfoCardListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_indagine);
 
-        IndagineHead indagineHead = getIntent().getParcelableExtra("Indagine");
-        this.setTitle(indagineHead.getTitoloIndagine());
+        //IndagineHead indagineHead = getIntent().getParcelableExtra("Indagine");
+        //this.setTitle(indagineHead.getTitoloIndagine());
+
+        RecyclerView rv = (RecyclerView)findViewById(R.id.infoScrollRecycleView);
+        rv.setHasFixedSize(true);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.HORIZONTAL);
+        rv.setLayoutManager(llm);
+
+        List<Informazione> informazioneList = getInformazioniList();
+
+        InformazioneAdapter adapter = new InformazioneAdapter(informazioneList, this);
+        rv.setAdapter(adapter);
+    }
+
+    private List<Informazione> getInformazioniList() {
+        List<Informazione> lista = new ArrayList<>();
+
+        lista.add(new Informazione("Informazione.pdf", "url_file", "document/pdf", "https://mangadex.org/images/avatars/default2.jpg"));
+        lista.add(new Informazione("Informazione.mp3", "url_file", "audio/mp3", "https://mangadex.org/images/avatars/default2.jpg"));
+        lista.add(new Informazione("Informazione.txt", "url_file", "document/txt", "https://mangadex.org/images/avatars/default2.jpg"));
+        lista.add(new Informazione("Informazione.avi", "url_file", "video/avi", "https://mangadex.org/images/avatars/default2.jpg"));
+        lista.add(new Informazione("Informazione.txt", "url_file", "document/txt", "https://mangadex.org/images/avatars/default2.jpg"));
+        lista.add(new Informazione("Informazione.mp4", "url_file", "document/pdf", "https://mangadex.org/images/avatars/default2.jpg"));
+
+        return lista;
+    }
+
+    @Override
+    public void onInfoCardClick(int position) {
+        Toast.makeText(this, getInformazioniList().get(position).getNomeFile(), Toast.LENGTH_LONG).show();
+
+
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try  {
+                    String file_path = downloadFile("http://www.lia.deis.unibo.it/Staff/LucaFoschini/htmlDocs/resources/laTex/scrivereTesiConLaTeX.pdf");
+                    openFile(file_path);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+    private String downloadFile(String url){
+        try {
+            URL u = new URL(url);
+            URLConnection urlcon = u.openConnection();
+            InputStream is = urlcon.getInputStream();
+
+            DataInputStream dis = new DataInputStream(is);
+
+            byte[] buffer = new byte[1024];
+            int length;
+
+            FileOutputStream fos = new FileOutputStream(new File( Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/informazione2.pdf"));
+            while ((length = dis.read(buffer))>0) {
+                fos.write(buffer, 0, length);
+            }
+            return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/informazione.pdf";
+
+        } catch (MalformedURLException mue) {
+            Log.e("SYNC getUpdate", "malformed url error", mue);
+            return null;
+        } catch (IOException ioe) {
+            Log.e("SYNC getUpdate", "io error", ioe);
+            return null;
+        } catch (SecurityException se) {
+            Log.e("SYNC getUpdate", "security error", se);
+            return null;
+        }
+    }
+
+    private void openFile(String filename){
+        File file = new File(filename);
+        Intent target = new Intent(Intent.ACTION_VIEW);
+        target.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        //Uri uri = FileProvider.getUriForFile(IndagineActivity.this, this.getApplicationContext().getPackageName() + ".provider", file);
+        target.setDataAndType(Uri.parse(filename),"application/pdf");
+        target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+
+        Intent intent = Intent.createChooser(target, "Apri con");
+        try {
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, "NOOOO SI SGHEEEE", Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ BICAP/app/src/main/java/com/example/bicap/InformazioneAdapter.java
@@ -0,0 +1,76 @@
+package com.example.bicap;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class InformazioneAdapter extends RecyclerView.Adapter<InformazioneAdapter.InformazioneViewHolder> {
+
+    private OnInfoCardListener onInfoCardListener;
+    private List<Informazione> informazioneList;
+
+    InformazioneAdapter(List<Informazione> informazioneList, OnInfoCardListener onInfoCardListener){
+        this.informazioneList = informazioneList;
+        this.onInfoCardListener = onInfoCardListener;
+    }
+
+    @Override
+    public int getItemCount(){
+        return informazioneList.size();
+    }
+
+    @Override
+    public InformazioneViewHolder onCreateViewHolder(ViewGroup viewGroup, int i){
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.square_info_card, viewGroup, false);
+        InformazioneViewHolder ivh = new InformazioneViewHolder(v, onInfoCardListener);
+        return ivh;
+    }
+
+    public void onBindViewHolder(@NonNull InformazioneViewHolder holder, int position) {
+        holder.informazioneTextView.setText(informazioneList.get(position).getNomeFile());
+        Glide.with(holder.informazioneImageView.getContext())
+                .load(informazioneList.get(position).getThumbnailUrl())
+                .centerCrop()
+                .placeholder(R.drawable.square_avatar_rounded)
+                .into(holder.informazioneImageView);
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    public static class InformazioneViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        ImageView informazioneImageView;
+        TextView informazioneTextView;
+        OnInfoCardListener onInfoCardListener;
+
+        public InformazioneViewHolder(View itemView, OnInfoCardListener onInfoCardListener){
+            super(itemView);
+            informazioneImageView = (ImageView) itemView.findViewById(R.id.informazioneImageView);
+            informazioneTextView = (TextView) itemView.findViewById(R.id.informazioneTextView);
+            this.onInfoCardListener = onInfoCardListener;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            onInfoCardListener.onInfoCardClick(getAdapterPosition());
+        }
+    }
+
+    public interface OnInfoCardListener{
+        void onInfoCardClick(int position);
+    }
+}
